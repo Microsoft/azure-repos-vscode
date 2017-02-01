6838f4e339e131122fd7cc8e2ab2703d8669e330
Jason Prickett <jpricket@microsoft.com>
2017-02-01 16:32:00 -0500

    Fixing the version output and adding tests

--- src/tfvc/commands/findworkspace.ts
+++ src/tfvc/commands/findworkspace.ts
@@ -4,9 +4,10 @@
 *--------------------------------------------------------------------------------------------*/
 "use strict";
 
-import {IArgumentProvider, IExecutionResult, ITfvcCommand, IWorkspace, IWorkspaceMapping } from "../interfaces";
+import { IArgumentProvider, IExecutionResult, ITfvcCommand, IWorkspace, IWorkspaceMapping } from "../interfaces";
 import { TfvcError } from "../tfvcerror";
 import { ArgumentBuilder } from "./argumentbuilder";
+import { CommandHelper } from "./commandhelper";
 
 /**
  * This command only returns a partial workspace object that allows you to get the name and server.
@@ -49,7 +50,7 @@ export class FindWorkspace implements ITfvcCommand<IWorkspace> {
         }
 
         // Find the workspace name and collectionUrl
-        const lines = stdout.replace("\r\n", "\n").split("\n");
+        const lines = CommandHelper.SplitIntoLines(stdout);
         let workspaceName: string = "";
         let collectionUrl: string = "";
         let equalsLineFound: boolean = false;
