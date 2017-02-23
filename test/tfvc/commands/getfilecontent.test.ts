7580356bbe71f35bbbbd48dd8aa3db2cd4778368
Jeff Young <jeyou@microsoft.com>
2017-02-23 14:50:41 -0500

    Great PR feedback!

--- test/tfvc/commands/getfilecontent.test.ts
+++ test/tfvc/commands/getfilecontent.test.ts
@@ -5,7 +5,6 @@
 "use strict";
 
 import { assert } from "chai";
-//import * as path from "path";
 import { GetFileContent } from "../../../src/tfvc/commands/getfilecontent";
 import { TfvcError } from "../../../src/tfvc/tfvcerror";
 import { IExecutionResult } from "../../../src/tfvc/interfaces";
@@ -61,8 +60,6 @@ describe("Tfvc-GetFileContentCommand", function() {
         assert.throws(() => new GetFileContent(undefined, undefined), TfvcError, /Argument is required/);
     });
 
-//TODO: Test when versionSpec is passed, TEst when ignore is passed
-
     it("should verify GetOptions", function() {
         let localPath: string = "/usr/alias/repos/Tfvc.L2VSCodeExtension.RC/README.md";
         let cmd: GetFileContent = new GetFileContent(undefined, localPath);
@@ -97,11 +94,6 @@ describe("Tfvc-GetFileContentCommand", function() {
         assert.equal(cmd.GetArguments().GetArgumentsForDisplay(), "print -noprompt -collection:" + collectionUrl + " ******** " + localPath + " -version:42");
     });
 
-//
-//
-//
-//
-//
     it("should verify GetExeOptions", function() {
         let localPath: string = "/usr/alias/repos/Tfvc.L2VSCodeExtension.RC/README.md";
         let cmd: GetFileContent = new GetFileContent(undefined, localPath);
@@ -229,153 +221,4 @@ describe("Tfvc-GetFileContentCommand", function() {
         }
     });
 
-    // it("should verify parse output - single empty folder - no errors", async function() {
-    //     let localPaths: string[] = ["empty-folder"];
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 0,
-    //         stdout: "empty-folder:\n" +
-    //                 "empty-folder\n",
-    //         stderr: undefined
-    //     };
-
-    //     let filesDeleted: string[] = await cmd.ParseOutput(executionResult);
-    //     assert.equal(filesDeleted.length, 1);
-    //     //In this case, the CLC returns:
-    //     //empty-folder:
-    //     //empty-folder
-    //     //So we will have to return empty-folder\empty-folder.  Not ideal but let's ensure we're getting that.
-    //     assert.equal(filesDeleted[0], path.join(localPaths[0], localPaths[0]));
-    // });
-
-    // it("should verify parse output - single folder+file - no errors", async function() {
-    //     let localPaths: string[] = [path.join("folder1", "file1.txt")];
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 0,
-    //         stdout: "folder1:\n" +
-    //                 "file1.txt\n",
-    //         stderr: undefined
-    //     };
-
-    //     let filesDeleted: string[] = await cmd.ParseOutput(executionResult);
-    //     assert.equal(filesDeleted.length, 1);
-    //     assert.equal(filesDeleted[0], localPaths[0]);
-    // });
-
-    // it("should verify parse output - single subfolder+file - no errors", async function() {
-    //     let localPaths: string[] = [path.join("folder1", "folder2", "file2.txt")];
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 0,
-    //         stdout: path.join("folder1", "folder2") + ":\n" +
-    //                 "file2.txt\n",
-    //         stderr: undefined
-    //     };
-
-    //     let filesDeleted: string[] = await cmd.ParseOutput(executionResult);
-    //     assert.equal(filesDeleted.length, 1);
-    //     assert.equal(filesDeleted[0], localPaths[0]);
-    // });
-
-    // it("should verify parse output - single folder+file - spaces - no errors", async function() {
-    //     let localPaths: string[] = [path.join("fold er1", "file1.txt")];
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 0,
-    //         stdout: "fold er1:\n" +
-    //                 "file1.txt\n",
-    //         stderr: undefined
-    //     };
-
-    //     let filesDeleted: string[] = await cmd.ParseOutput(executionResult);
-    //     assert.equal(filesDeleted.length, 1);
-    //     assert.equal(filesDeleted[0], localPaths[0]);
-    // });
-
-    // it("should verify parse output - multiple files", async function() {
-    //     let noChangesPaths: string[] = [path.join("folder1", "file1.txt"), path.join("folder2", "file2.txt")];
-    //     let localPaths: string[] = noChangesPaths;
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 0,
-    //         stdout: path.join("folder1", "file1.txt") + ":\n" +
-    //             "file1.txt\n" +
-    //             path.join("folder2", "file2.txt") + ":\n" +
-    //             "file2.txt\n",
-    //         stderr: undefined
-    //     };
-
-    //     let filesDeleted: string[] = await cmd.ParseOutput(executionResult);
-    //     assert.isDefined(filesDeleted);
-    //     assert.equal(filesDeleted.length, 2);
-    // });
-
-    // it("should verify parse output - error exit code", async function() {
-    //     let noChangesPaths: string[] = [path.join("folder1", "file1.txt"), path.join("folder2", "file2.txt")];
-    //     let localPaths: string[] = noChangesPaths;
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 42,
-    //         stdout: "Something bad this way comes.",
-    //         stderr: undefined
-    //     };
-
-    //     try {
-    //         await cmd.ParseOutput(executionResult);
-    //     } catch (err) {
-    //         assert.equal(err.exitCode, 42);
-    //         assert.equal(err.tfvcCommand, "delete");
-    //         assert.equal(err.message.indexOf("Something bad this way comes."), 0);
-    //         assert.equal(err.stdout.indexOf("Something bad this way comes."), 0);
-    //     }
-    // });
-
-    // // TF203069: $/L2.VSCodeExtension.RC/folder1/folder2 could not be deleted because that change conflicts with one or more other pending changes to that item. To delete it, undo all pending changes to that item, delete it, and then check in the change.
-    // // No arguments matched any files to delete.
-    // it("should verify parse output - deletion conflict - error exit code", async function() {
-    //     let noChangesPaths: string[] = [path.join("folder1", "folder2")];
-    //     let localPaths: string[] = noChangesPaths;
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 100,
-    //         stdout: "// TF203069: $/L2.VSCodeExtension.RC/folder1/folder2 could not be deleted because that change " +
-    //             "conflicts with one or more other pending changes to that item. To delete it, undo all pending changes " +
-    //             "to that item, delete it, and then check in the change.\n" +
-    //             "No arguments matched any files to delete.\n",
-    //         stderr: undefined
-    //     };
-
-    //     try {
-    //         await cmd.ParseOutput(executionResult);
-    //     } catch (err) {
-    //         assert.equal(err.exitCode, 100);
-    //         assert.equal(err.tfvcCommand, "delete");
-    //         assert.isAtLeast(err.stdout.indexOf("TF203069: "), 0);
-    //         assert.isAtLeast(err.stdout.indexOf("No arguments matched any files to delete"), 0);
-    //     }
-    // });
-
-    // //The item C:\repos\Tfvc.L2VSCodeExtension.RC\folder1\folder2\foo.txt could not be found in your workspace, or you do not have permission to access it.
-    // //No arguments matched any files to delete.
-    // it("should verify parse output - delete a file that doesn't exist - error exit code", async function() {
-    //     let noChangesPaths: string[] = [path.join("folder1", "folder2", "foo.txt")];
-    //     let localPaths: string[] = noChangesPaths;
-    //     let cmd: Delete = new Delete(undefined, localPaths);
-    //     let executionResult: IExecutionResult = {
-    //         exitCode: 100,
-    //         stdout: "The item C:\\repos\\Tfvc.L2VSCodeExtension.RC\\folder1\\folder2\\foo.txt could not be found in your workspace, or you do not have permission to access it.\n" +
-    //             "No arguments matched any files to delete.\n",
-    //         stderr: undefined
-    //     };
-
-    //     try {
-    //         await cmd.ParseOutput(executionResult);
-    //     } catch (err) {
-    //         assert.equal(err.exitCode, 100);
-    //         assert.equal(err.tfvcCommand, "delete");
-    //         assert.isAtLeast(err.stdout.indexOf("could not be found in your workspace"), 0);
-    //         assert.isAtLeast(err.stdout.indexOf("No arguments matched any files to delete"), 0);
-    //     }
-    // });
 });
